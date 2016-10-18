0b8d0968a19e212b3e179b96643f164d89c2a3e6
Théo Leffyr <Tleffyr@MacBook-Pro-de-Theo.local>
2016-10-18 16:12:21 +0200

    Ajoute un petit commentaire

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
+    //Theo was here
     std::cout << "Hello World" << std::endl;
     std::cout << "My very first commit" << std::endl;
 
