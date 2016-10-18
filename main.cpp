c2de703fc419668ebdb7e6fa1c00a5f64ec9d0bf
PCTIMOTHE\Tim <timothe.mercat@gmail.com>
2016-10-18 15:58:06 +0200

    My very first commit

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
 
     std::cout << "Hello World" << std::endl;
+    std::cout << "My very first commit" << std::endl;
 
 
     return a.exec();
