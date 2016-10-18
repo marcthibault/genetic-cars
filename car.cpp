64295fa30499f6a33e0700191d93d0e2adf746a2
Aymeri GALLAIS <Aymeri GALLAIS>
2016-10-18 14:23:14 +0200

    Created a QT project

--- car.cpp
+++ car.cpp
@@ -1,6 +1,7 @@
 #include "car.h"
 #include <vector>
 #include <pair>
+#include <iostream>
 
 using namespace std;
 
@@ -13,12 +14,13 @@ Car::Car()
     vector<pair<double, double>> haut;
     vector<pair<double, double>> bas;
 
-    Car::Car(double rayon1, double rayon2){
+    Car::Car( double rayon1, double rayon2)
+    {
 
       this->rayon1 = rayon1;
       this->rayon2 = rayon2;
       haut = std::vector<pair<double,double>();
-
+    std::cout<<"Hello World"<<std::endl;
     }
 
 
