80e6a753113743f3befdb803e4a7f08a4bbcd050
Théo Leffyr <Tleffyr@MacBook-Pro-de-Theo.local>
2016-11-08 15:54:06 +0100

    Complète la génération de vecteurs gaussiens, les fonctions de conversion de car en vector

--- main.cpp
+++ main.cpp
@@ -3,13 +3,13 @@
 #include <iostream>
 
 
-
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     std::cout << "Theo was here" << std::endl;
     std::cout << "Enfin !!" <<std::endl;
     std::cout << "Hello World" << std::endl;
+
 
 
     return a.exec();
--- preprocessing.cpp
+++ preprocessing.cpp
@@ -1,10 +1,8 @@
 #include "preprocessing.h"
 #include "car.h"
-<<<<<<< HEAD
 #include <math.h>
-=======
->>>>>>> 89b2f883818cbe51f0792d038fc7b7953dbafc3e
 #include <vector>
+#include <random>
 
 preprocessing::preprocessing()
 {
@@ -12,12 +10,39 @@ preprocessing::preprocessing()
 }
 
 // Transform an object Car in array
-std::vector<double> preprocessing::open(car my_car){
+std::vector<double> preprocessing::openCar(Car my_car){
     std::vector<double> data;
-    data.push_back(my_car.a);
+    data.push_back(my_car.rayon1);
+    data.push_back(my_car.rayon2);
+    data.push_back(my_car.L);
+    data.push_back(my_car.densiteRoue1);
+    data.push_back(my_car.densiteRoue2);
+    data.push_back(my_car.densiteVoiture);
+    for(int i=0;i<my_car.angles.size();i++){
+       data.push_back(my_car.angles[i]);
+    }
+    for(int i=0;i<my_car.distances.size();i++){
+       data.push_back(my_car.distances[i]);
+    }
 
     return data;
 }
+Car preprocessing::returnCar(std::vector<double> attributes){
+    Car my_car = Car();
+    my_car.rayon1 = attributes[0];
+    my_car.rayon1 = attributes[1];
+    my_car.L = attributes[2];
+    my_car.densiteRoue1 = attributes[3];
+    my_car.densiteRoue2 = attributes[4];
+    my_car.densiteVoiture = attributes[5];
+    int len = (attributes.size() - 6)/2;
+    for(int i = 0; i < len; i++){
+        my_car.angles.push_back(attributes[6+i]);
+        my_car.distances.push_back(attributes[6+len+i]);
+    }
+    return my_car;
+}
+
 // Compute the coeff associated to tha ranking of the car
 double preprocessing::computeCoeff(int rank, int total){
     if(rank < total){
@@ -28,17 +53,21 @@ double preprocessing::computeCoeff(int rank, int total){
 }
 // Function that takes the ranking of the race and generates the coefficients
 std::vector<double> preprocessing::generateCoeff(int N){
-    vector<double> coeffs = new vector<double>();
-    for (i=0;i<N;i++){
-       coeffs.push_back(computeCoeffs(i));
+    std::vector<double> coeffs;
+    for (int i=0;i<N;i++){
+       coeffs.push_back(computeCoeff(i, N));
     }
     return coeffs;
 }
 // Compute the random evolution of the new cars
-void preprocessing::computeRandomVector(car my_car){
-    std::vector<double> attributes = open(my_car);
+void preprocessing::computeRandomVector(Car my_car){
+    std::vector<double> attributes = openCar(my_car);
+    std::default_random_engine generator;
     for(int i=0;i<attributes.size();i++){
-        attributes[i];
+        //std::normal_distribution<double> distribution(5.0,2.0);
+        std::normal_distribution<double> distribution(attributes[i],attributes[i]/10);
+        double variation = distribution(generator);
+        attributes[i] += variation;
     }
 }
 
