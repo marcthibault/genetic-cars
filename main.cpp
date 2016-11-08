7765507554bdb138390e962c369af548c2868b39
gregory.calvez <gregory.calvez@hotmail.com>
2016-11-08 15:13:27 +0100

    Test Voiture

--- main.cpp
+++ main.cpp
@@ -13,30 +13,74 @@ int main(int argc, char *argv[])
     groundBodyDef.position.Set(0.0f, -10.0f);
     b2Body* groundBody = world.CreateBody(&groundBodyDef);
     b2PolygonShape groundBox;
-    groundBox.SetAsBox(50.0f, 10.0f);
+    groundBox.SetAsBox(500.0f, 100.0f);
     groundBody->CreateFixture(&groundBox, 0.0f);
 
-    b2BodyDef bodyDef;
-    bodyDef.type = b2_dynamicBody;
-    bodyDef.position.Set(0.0f, 4.0f);
-    b2Body* body = world.CreateBody(&bodyDef);
+    b2BodyDef bd;
+    bd.type = b2_dynamicBody;
+    bd.position.Set(0.0f, 4.0f);
+    b2Body* car = world.CreateBody(&bd);
     b2PolygonShape dynamicBox;
-    dynamicBox.SetAsBox(1.0f, 1.0f);
+    dynamicBox.SetAsBox(4.0f, 2.0f);
     b2FixtureDef fixtureDef;
     fixtureDef.shape = &dynamicBox;
     fixtureDef.density = 1.0f;
     fixtureDef.friction = 0.3f;
-    body->CreateFixture(&fixtureDef);
+    car->CreateFixture(&fixtureDef);
+
+
+    b2CircleShape circle;
+    circle.m_radius = 1.0f;
+    b2FixtureDef fd;
+    fd.shape = &circle;
+    fd.density = 1.0f;
+    fd.friction = 0.9f;
+
+    b2Body* wheel1;
+    b2Body* wheel2;
+
+    bd.position.Set(2.0f, 2.0f);
+    wheel1 = world.CreateBody(&bd);
+    wheel1->CreateFixture(&fd);
+
+    bd.position.Set(-2.0f, -2.0f);
+    wheel2 = world.CreateBody(&bd);
+    wheel2->CreateFixture(&fd);
+
+    b2WheelJointDef joint;
+    b2Vec2 axe(0.0f, 1.0f);
+
+    joint.Initialize(car, wheel1, wheel1->GetPosition(), axe);
+    joint.motorSpeed = 0.5f;
+    joint.maxMotorTorque = 20.0f;
+    joint.enableMotor = true;
+    joint.frequencyHz = 4.0f;
+    joint.dampingRatio = 0.5f;
+
+
+    b2WheelJoint* liaisonAvant;
+    b2WheelJoint* liaisonArriere;
+
+    liaisonAvant = (b2WheelJoint*)world.CreateJoint(&joint);
+
+    joint.Initialize(car, wheel2, wheel2->GetPosition(), axe);
+    joint.motorSpeed = 0.5f;
+    joint.maxMotorTorque = 20.0f;
+    joint.enableMotor = false;
+    joint.frequencyHz = 4.0f;
+    joint.dampingRatio = 0.5f;
+
+    liaisonArriere = (b2WheelJoint*)world.CreateJoint(&joint);
 
 
     float32 timeStep = 1.0f / 60.0f;
     int32 velocityIterations = 6;
     int32 positionIterations = 2;
-    for (int32 i = 0; i < 60; ++i)
+    for (int32 i = 0; i < 600; ++i)
     {
         world.Step(timeStep, velocityIterations, positionIterations);
-        b2Vec2 position = body->GetPosition();
-        float32 angle = body->GetAngle();
+        b2Vec2 position = wheel2->GetPosition();
+        float32 angle = wheel2->GetAngle();
         std::cout << " X: " << position.x << " \t Y : " << position.y << " \t Angle : " << angle << std::endl;
     }
     return 0;
