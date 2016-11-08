dc212cdec6a6176dd0f56c2ae7dc173976ef5c94
lucas zins <lucas.zins@polytechnique.fr>
2016-11-08 13:46:54 +0100

    Added car.h and car.cpp

--- car.cpp
+++ car.cpp
@@ -6,7 +6,12 @@
 using namespace std;
 
 
-Car::Car( double rayon1, double rayon2) : rayon1(rayon1), rayon2(rayon2) {
+Car(double rayon1, double rayon2, double L, double yO, double densiteRoue1, double densiteRoue2,
+    double densiteVoiture, vector<double> angles, vector<double> distances)
+    : rayon1(rayon1), rayon2(rayon2), L(L), yO(yO), densiteRoue1(densiteRoue1), densiteRoue2(densiteRoue2),
+      densiteVoiture(densiteVoiture), angles(angles){}
+
+Car::Car(){
 
 }
 
--- car.h
+++ car.h
@@ -3,20 +3,24 @@
 
 #include <vector>
 
-#include<utility>
-
 using namespace std;
 
 class Car
 {
 public :
+    static int nNoeuds = 10; // nombre total de noeuds
     double rayon1;
     double rayon2;
-    vector<pair<double, double> > haut;
-    vector<pair<double, double> > bas;
+    double L; // distance entre les roues
+    double yO; // ordonnee du point O
+    double densiteRoue1;
+    double densiteRoue2;
+    double densiteVoiture;
 
+    vector<double> angles;
+    vector<double> distances; // tailles des rayons
     Car();
-    Car(double rayon1, double rayon2);
+    Car(double rayon1, double rayon2, double L, double yO, double densiteRoue1, double densiteRoue2, double densiteVoiture, vector<double> angles, vector<double> distances);
 };
 
 #endif // CAR_H
