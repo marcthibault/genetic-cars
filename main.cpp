631a8e940bf7d3fed9eb7a03055ba01b5ad3441b
Louis Rémus <louisremus@gmail.com>
2016-11-08 17:42:01 +0100

    Petit commit

--- main.cpp
+++ main.cpp
@@ -9,8 +9,23 @@ int main(int argc, char *argv[])
     std::cout << "Theo was here" << std::endl;
     std::cout << "Enfin !!" <<std::endl;
     std::cout << "Hello World" << std::endl;
+    std::vector<double> angles =  std::vector<double>();
+    angles.push_back(1.0);
+    angles.push_back(2.0);
 
+    std::vector<double> distances =  std::vector<double>();
+    distances.push_back(1.0);
+    distances.push_back(2.0);
 
+    Car * my_car = new Car::Car(1.0,
+                         2.0,
+                         5.0,
+                         1.0,
+                         1.0,
+                         1.0,
+                         angles,
+                         distances);
 
+    std::cout << my_car->rayon1 << std::endl;
     return 0;
 }
