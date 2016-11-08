c3894be58dc7eb0812cb52e07ee528d234633074
Michel Deudon <michel.deudon@hotmail.com>
2016-11-08 18:22:47 +0100

    debugage (okayyy !)

--- car.cpp
+++ car.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <math.h>
 #include <time.h>
+#include <algorithm>
 
 using namespace std;
 
@@ -12,9 +13,6 @@ static double D_max = 4;//Distance entre les deux roues maximale
 static double d_max = 1;//Densité maximale
 static double distance_max = 6;//Distance des points au centre maximale
 static int N=8;//Nombre de sommets du polygone
-
-Car::Car(double r1, double d1, double r2, double d2, double D, double d, vector<pair<double, double> > angles_distances)
-    : r1(r1), d1(d1), r2(r2), d2(d2), D(D), d(d), angles_distances(angles_distances){}
 
 Car::Car(){
     srand(time(0));
@@ -35,44 +33,45 @@ Car::Car(){
     ordered = false;
 }
 
-void Car::order_angles(){
-    sort(angles_distances.begin(), angles_distances.end(), Car::pair_inf);
-    ordered = true;
+Car::Car(double r1, double d1, double r2, double d2, double D, double d, vector<pair<double, double> > angles_distances)
+    : r1(r1), d1(d1), r2(r2), d2(d2), D(D), d(d), angles_distances(angles_distances){}
+
+double Car::get_angle_wheel(){ //angle théta (cf schéma)
+    return atan(2*(this->r1)/(this->D));
 }
 
-vector<pair<double, double> > get_points(){ //renvoie les sommets du polygones (dans l'ordre trigonométrique)
-    if(!this->ordered) {
-        this->order();
+vector<int> Car::get_wheels_index(){
+    double theta = get_angle_wheel();
+    vector<int> wheels_index;
+    wheels_index[0] = 0; //roue arrière
+    wheels_index[1] = 7; //roue avant
+    for(int i=0;i<N-2;i++){
+        if (std::get<0>(angles_distances[i])<M_PI+theta) wheels_index[0]++;
+        else if (std::get<0>(angles_distances[i])>2*M_PI-theta) wheels_index[1]--;
     }
-    vector<pair<double,double>> points();
-    for (auto a_d : angles_distances){
-        points.push_back(pair<double,double>(a_d.get<1>()*cos(a_d.get<0>()), a_d.get<1>()*sin(a_d.get<1>())));
-    }
-    return points;
+    return  wheels_index;
 }
 
-bool violate_constraint(double candidate,vector<double> S){
+bool Car::violate_constraint(double candidate,vector<double> S){
     for (std::vector<double>::iterator i=S.begin();i!=S.end();i++){
         if (candidate==(*i)) return true;
     }
     return false;
 }
 
-Car::get_angle_wheel(){
-    double angle_wheel = atan(D/(2*r1));
-    return angle_wheel;
-}
 
-Car::get_wheels_index(){
-    vector<int> wheels_index;
-    wheels_index[0] = 0;
-    wheels_index[1] = 8;
-    double theta = get_angle_wheel();
-    for(int i=0;i<N-2;i++){
-        if (angles_distances[i]<M_PI+theta) wheels_index[0]++;
-        else if (angles_distances[i>2*M_PI-theta]) wheels_index[1]--;
-    }
-    return  wheels_index;
+vector<pair<double, double> > Car::get_points(){ //renvoie les sommets du polygones (dans l'ordre trigonométrique)
+    vector<pair<double,double>> points;
+
+    double theta=this->get_angle_wheel();
+    double RC=sqrt(r1*r1+(D/2)*(D/2)); //distance from center C to wheel R
+    points.push_back(pair<double,double>(M_PI+theta, RC));
+    points.push_back(pair<double,double>(2*M_PI-theta, RC));
+
+    for (auto a_d : angles_distances){points.push_back(a_d);}
+
+    std::sort(points.begin(),points.end());
+    return points;
 }
 
 
