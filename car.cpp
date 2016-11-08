4a3e11e8331faa925caa1200f011e559457436d4
Michel Deudon <michel.deudon@hotmail.com>
2016-11-08 16:17:16 +0100

    fonction violate

--- car.cpp
+++ car.cpp
@@ -3,6 +3,7 @@
 #include <utility>
 #include <iostream>
 #include <math.h>
+#include <time.h>
 
 using namespace std;
 
@@ -10,10 +11,10 @@ static double r_max = 1;//Rayon maximal
 static double D_max = 4;//Distance entre les deux roues maximale
 static double d_max = 1;//Densité maximale
 static double distance_max = 6;//Distance des points au centre maximale
+static int N=8;//Nombre de sommets du polygone
 
 
-
-Car(double r1, double d1, double r2, double d2, double D, double d, vector<pair<double, double> > angles_distances)
+Car::Car(double r1, double d1, double r2, double d2, double D, double d, vector<pair<double, double> > angles_distances)
     : r1(r1), d1(d1), r2(r2), d2(d2), D(D), d(d), angles_distances(angles_distances){}
 
 Car::Car(){
@@ -25,11 +26,20 @@ Car::Car(){
     D = (double) rand() / RAND_MAX * D_max;
     d = (double) rand() / RAND_MAX * d_max;
 
+    vector<pair<double, double> > angles_distances;
+
     for(int i=0; i<N-2; i++){
-        angle =  (double) rand() / RAND_MAX * 2 * M_PI;
-        distance = (double) rand() / RAND_MAX * distance_max;
-        angles_distances[i] = pair<double, double> (angles,distance);
+        double angle =  (double) rand() / RAND_MAX * 2 * M_PI;
+        double distance = (double) rand() / RAND_MAX * distance_max;
+        angles_distances.push_back(pair<double, double> (angle,distance));
     }
 }
 
+bool violate_constraint(double candidate,vector<double> S){
+    for (std::vector<double>::iterator i=S.begin();i!=S.end();i++){
+        if (candidate==(*i)) return true;
+    }
+    return false;
+}
 
+Car Jeep= Car();
--- car.h
+++ car.h
@@ -15,14 +15,13 @@ public :
     double d2; //densité roue arrière
     double D; //distance entre les deux roues
     double d; //densité de la carrosserie
-    static int N=8; //nombre de sommets du polygone (carrosserie)
     vector<pair<double, double> > angles_distances; //coordonées polaires des points A3,...AN
 
     Car(); //constructeur (random)
     Car(double r1, double d1, double r2, double d2, double D, double d, vector<pair<double, double> > angles_distances); //constructeur
 
     double get_angle_wheel(); //cf schéma (angle théta)
-    bool check_constraint(); //vérifie si l'angle candidat ne coincide pas avec les roues
+    bool violate_constraint(double candidate,vector<double> S); //renvoie True si l'angle candidat existe déjà dans S!
     vector<pair<double, double> > get_points(); //renvoie les sommets du polygones (dans l'ordre trigonométrique)
     vector<int> get_wheels_index(); //renvoie les positions des roues avant et arrière dans le polygone
 };
