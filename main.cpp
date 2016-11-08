2c2acd92687e8a2a7f0f7bbb1d7efa45defb64b2
gregory.calvez <gregory.calvez@hotmail.com>
2016-11-08 14:17:24 +0100

    Test Box2D

--- main.cpp
+++ main.cpp
@@ -1,16 +1,39 @@
 #include <QCoreApplication>
-#include "car.h"
 #include <iostream>
+#include <Box2D/Box2D.h>
 
 
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
-    std::cout << "Theo was here" << std::endl;
-
-    std::cout << "Hello World" << std::endl;
-
-
-    return a.exec();
+    b2Vec2 gravity(0.0f, -10.0f);
+    b2World world(gravity);
+    b2BodyDef groundBodyDef;
+    groundBodyDef.position.Set(0.0f, -10.0f);
+    b2Body* groundBody = world.CreateBody(&groundBodyDef);
+    b2PolygonShape groundBox;
+    groundBox.SetAsBox(50.0f, 10.0f);
+    groundBody->CreateFixture(&groundBox, 0.0f);
+    b2BodyDef bodyDef;
+    bodyDef.type = b2_dynamicBody;
+    bodyDef.position.Set(0.0f, 4.0f);
+    b2Body* body = world.CreateBody(&bodyDef);
+    b2PolygonShape dynamicBox;
+    dynamicBox.SetAsBox(1.0f, 1.0f);
+    b2FixtureDef fixtureDef;
+    fixtureDef.shape = &dynamicBox;
+    fixtureDef.density = 1.0f;
+    fixtureDef.friction = 0.3f;
+    body->CreateFixture(&fixtureDef);
+    float32 timeStep = 1.0f / 60.0f;
+    int32 velocityIterations = 6;
+    int32 positionIterations = 2;
+    for (int32 i = 0; i < 60; ++i)
+    {
+        world.Step(timeStep, velocityIterations, positionIterations);
+        b2Vec2 position = body->GetPosition();
+        float32 angle = body->GetAngle();
+        std::cout << " X: " << position.x << " \t Y : " << position.y << " \t Angle : " << angle << std::endl;
+    }
+    return 0;
 }
